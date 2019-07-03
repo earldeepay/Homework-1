0087a9a2f774b4bb1305e0c491e074fa07815b0c
Earl Deepay <37359509+earldeepay@users.noreply.github.com>
2019-07-03 20:09:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  3 19:25:05 2019
+
+@author: DELL PC
+"""
+
+Song = "Old Town Road"
+Artist =  "Lil Nas X"
+Album = "So Fresh: The Hits of Winter 2019"
+Released = 2019    #release date is in integer
+Genres = "Country rap, Hip-Hop/Rap" #the genres are in string
+Nominations = "Nomination:3"
+other = "billbord No.1" 
+Youtube = '3.8M views'      #as we have added Double quotation there will be no error in executing the program. 
+duration = 2.37    #in this we have used float
+""" these statements below are used to print the given information above"""
+print(Song)                     
+print(Artist)
+print(Album)
+print(Released)
+print(Genres)
+print(Nominations)
+print(other)
+print(Youtube)
+print(duration)
